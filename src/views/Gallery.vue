3e0b77fd98c5c6c5ce636b753a1e3ae938abdcd9
txk-productionz <tylerxvu@gmail.com>
2020-05-16 21:06:46 -0400

    test

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="res-width view">
         <h3>Gallery</h3>
-        <p>Welcome to the gallery page. Photos coming soon...</p>
+        <p>Welcome, photos coming soon...</p>
 
     </div>
 </template>
