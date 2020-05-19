74faa31da8dea05e7b01d367cc746dcecbdfd386
txk-productionz <tylerxvu@gmail.com>
2020-05-19 10:31:33 -0400

    new photo and style lightbox

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="view res-width lightbox">
     <p class="text-center mx-auto exitbtn" @click.self="closeLightbox">&#8249;Back to Gallery&#8250;</p>
-    <h4 class="text-center" v-if="photo.caption">"{{ photo.caption }}"</h4>
-    <p class="text-center" v-if="photo.location">{{ photo.location }}</p>
+    <h4 class="text-center border-top border-bottom py-3" v-if="photo.caption">"{{ photo.caption }}"</h4>
     <p class="text-center" v-if="photo.products">{{ photo.products }}</p>
     <p class="text-center" v-if="photo.credit">{{ photo.credit }}</p>
     <div class="d-flex position-fixed res-width ">
@@ -38,7 +37,7 @@ export default {
     },
     prevLightbox() {
         var nextid=this.photo.id+1
-        if (this.photo.id < 11) {    //<---Number should be one less than the number of images
+        if (this.photo.id < 12) {    //<---Number should be one less than the number of images
             this.$router.push('/photo/' + nextid)
         } 
     },
