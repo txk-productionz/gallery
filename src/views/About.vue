ab8246ff813a82120876432045df07b4a235cc53
txk-productionz <tylerxvu@gmail.com>
2020-05-29 21:33:04 -0400

    update new sponsorship

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,10 +16,17 @@
     passion for design and photography inspired our ideas. We wanted to share our passion
     with our friends so TXK (Tyler and Kyle) Productionz was created in April 2020.
     </p>
-    <h4>Sponsorship</h4>
-    <p class="mb-4">
+    <h4>Sponsorships</h4>
+    <p>
     Many of the watches shown are a courtesy of our sponsor who is a private watch collector. 
     This sponsorship allows us to feature a wide variety of watches from names such as Rolex, Seiko, Omega, and more.
+    </p>
+    <p class="mt-4">
+    We are also sponsored by a private sneaker collector who gives us access to sneakers from brands like Nike and Adidas.
+    </p>
+    <h4>Services</h4>
+    <p>
+      Some of the products featured may be for sale (open box, some new and some used). Please contact us for more details if you are interested.
     </p>
     <p>If you are interested in sending products for us to shoot, feel free to contact us.</p>
   </div>
