16997224a7d641cc6a0769e02876a570bef3f78d
txk-productionz <tylerxvu@gmail.com>
2020-05-29 22:39:41 -0400

    edit bio

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,22 +11,15 @@
     </p>
     <h4>Our Story: Two Brothers and a Camera</h4>
     <p class="mb-4">
-    We are two brothers, Tyler and Kyle, and are based out of Hartford, Connecticut 
+    We are two brothers based out of Hartford, Connecticut 
     in our parents basement. Both of us are college students studying engineering. Our 
     passion for design and photography inspired our ideas. We wanted to share our passion
-    with our friends so TXK (Tyler and Kyle) Productionz was created in April 2020.
+    with our friends so TXK Productionz was created in April 2020.
     </p>
     <h4>Sponsorships</h4>
-    <p>
+    <p class="mb-4">
     Many of the watches shown are a courtesy of our sponsor who is a private watch collector. 
     This sponsorship allows us to feature a wide variety of watches from names such as Rolex, Seiko, Omega, and more.
-    </p>
-    <p class="mt-4">
-    We are also sponsored by a private sneaker collector who gives us access to sneakers from brands like Nike and Adidas.
-    </p>
-    <h4>Services</h4>
-    <p>
-      Some of the products featured may be for sale (open box, some new and some used). Please contact us for more details if you are interested.
     </p>
     <p>If you are interested in sending products for us to shoot, feel free to contact us.</p>
   </div>
