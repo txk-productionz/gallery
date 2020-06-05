77534dff83a97cb4b770b7c7c641fd792419747f
txk-productionz <tylerxvu@gmail.com>
2020-06-05 12:33:37 -0400

    fix lightbox max page id count

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -42,7 +42,7 @@ export default {
     },
     prevLightbox() {
         var nextid=this.photo.id+1
-        if (this.photo.id < 18) {    //<---Number should be one less than the number of images
+        if (this.photo.id < 19) {    //<---Number should be one less than the number of images
             this.$router.push('/photo/' + nextid)
         } 
     },
